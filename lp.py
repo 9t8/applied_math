dd6080466caf203a4a84b108810e631cb11b253c
9t8 <63334933+9t8@users.noreply.github.com>
2023-09-11 09:46:06 -0700

    add reduced cost

--- lp.py
+++ lp.py
@@ -13,9 +13,9 @@ def analyze(f):
 
   print(f'Objective value: {s.Objective().Value()}')
 
-  print(f'{"Value":12}  Variable name')
+  print(f'{"Value":12}  {"Reduced cost":12}  Variable name')
   for var in s.variables():
-    print(f'{var.solution_value():12.6}  {var}')
+    print(f'{var.solution_value():12.6}  {var.reduced_cost():12.6}  {var}')
 
   print(f'{"Value":12}  {"Shadow price":12}  {"Upper bound":12}  Constraint name')
   for cons, val in zip(s.constraints(), s.ComputeConstraintActivities()):
