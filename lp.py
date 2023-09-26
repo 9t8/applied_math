be0a2589ceab4096d7927c8f911c689848d4a469
9t8 <63334933+9t8@users.noreply.github.com>
2023-09-25 17:43:33 -0700

    align

--- lp.py
+++ lp.py
@@ -14,11 +14,11 @@ def analyze(f):
   o = s.Objective()
   print(f'Objective value: {o.Value()}')
 
-  print(f'\n{"Value":12}  {"Reduced cost":12}  {"Obj. coef.":12}  Variable name')
+  print(f'\n{"Value":>12} {"Reduced cost":>12} {"Coefficient":>12} Variable name')
   for var in s.variables():
-    print(f'{var.solution_value():12.6}  {var.reduced_cost():12.6}  {o.GetCoefficient(var):12.6}  {var}')
+    print(f'{var.solution_value():12.6} {var.reduced_cost():12.6} {o.GetCoefficient(var):12.6} {var}')
 
-  print(f'\n{"Value":12}  {"Shadow price":12}  {"Bound":12}  Constraint name')
+  print(f'\n{"Value":>12} {"Shadow price":>12} {"Bound":>12} Constraint name')
   for cons, val in zip(s.constraints(), s.ComputeConstraintActivities()):
     finite = lambda x : -pywraplp.Solver.infinity() < x < pywraplp.Solver.infinity()
     if finite(cons.lb()):
@@ -26,4 +26,4 @@ def analyze(f):
     else:
       bound = cons.ub() if finite(cons.ub()) else 'NO BOUNDS'
 
-    print(f'{val:12.6}  {cons.dual_value():12.6}  {bound:12}  {cons.name()}')
+    print(f'{val:12.6} {cons.dual_value():12.6} {bound:>12} {cons.name()}')
