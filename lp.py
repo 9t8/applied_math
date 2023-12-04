d868c7cbef198f55184780ab5a8875194cc55c6a
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-03 23:12:04 -0800

    isolate pulp

--- lp.py
+++ lp.py
@@ -1,21 +1,4 @@
-import pulp
 from ortools.linear_solver import pywraplp
-
-def pulp_analyze(p):
-  p.solve()
-
-  print(f'Status: {pulp.LpStatus[p.status]}')
-  print(f'{p.name} = {pulp.value(p.objective)}')
-
-  print('Variables')
-  print(f'{"Value":>12} {"Reduced cost":>12} {"Coefficient":>12} Name')
-  for v in p.variables():
-      print(f'{pulp.value(v):12} {v.dj:12.6} {p.objective[v]:12} {v}')
-
-  print('Constraints')
-  print(f'{"Value":>12} {"Shadow price":>12} {"Bound":>12} Name')
-  for c in p.constraints.values():
-    print(f'{-c.constant - c.slack:12.6} {c.pi:12} {-c.constant:12} {c.name}')
 
 def ortools_analyze(f, solver_id='GLOP'):
   s = pywraplp.Solver.CreateSolver(solver_id)
--- skimboards.py
+++ skimboards.py
@@ -14,7 +14,20 @@ p += 2/3*a + 1/2*b <= 120, 'saw time_(hours)'
 p += a <= 100, 'resin_(skimboards)'
 p += 2*b <= 400, 'sanding_(hours)'
 
-lp.pulp_analyze(p)
+p.solve()
+
+print(f'Status: {pulp.LpStatus[p.status]}')
+print(f'{p.name} = {pulp.value(p.objective)}')
+
+print('Variables')
+print(f'{"Value":>12} {"Reduced cost":>12} {"Coefficient":>12} Name')
+for v in p.variables():
+    print(f'{pulp.value(v):12} {v.dj:12.6} {p.objective[v]:12} {v}')
+
+print('Constraints')
+print(f'{"Value":>12} {"Shadow price":>12} {"Bound":>12} Name')
+for c in p.constraints.values():
+  print(f'{-c.constant - c.slack:12.6} {c.pi:12} {-c.constant:12} {c.name}')
 
 def f(s):
   a = s.NumVar(0, s.infinity(), 'skimboards')
