e7bda6461e41d217e47e8d11a6f941159f4a93f5
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-20 00:05:08 -0800

    works

--- fairy.py
+++ fairy.py
@@ -4,17 +4,31 @@ import pandas as pd
 
 import math
 
-with open('fairy_chart.csv') as t:
-  df = pd.read_csv(t)
+with open('fairy_chart.csv') as file:
+  df = pd.read_csv(file)
 
-print(df.at[df['Attacking'] == 'Normal', 'Steel'])
 types = *(t for t in df if t != 'Attacking'),
-print(types)
 
-# coefs = {}
-# for att_type in types:
-#   coefs[att_type] = {
-#     def_type: math.log2((df[df['Attacking'] == att_type][def_type] + .2)
-#                         / (df[df['Attacking'] == def_type][att_type] + .2))
-#     for def_type in types
-#   }
+coefs = {}
+for att_type in types:
+  coefs[att_type] = {
+    def_type: math.log2((df[df['Attacking'] == att_type][def_type].iat[0] + .2)
+                        / (df[df['Attacking'] == def_type][att_type].iat[0] + .2))
+    for def_type in types
+  }
+
+def f(s):
+  x = {type: s.NumVar(0, s.infinity(), type) for type in coefs}
+  e = s.NumVar(-s.infinity(), s.infinity(), 'E')
+
+  s.Maximize(e)
+
+  s.Add(sum(x.values()) == 1, 'probabilities')
+
+  for opp_type in coefs:
+    s.Add(
+      sum(coefs[my_type][opp_type] * x[my_type] for my_type in coefs) >= e,
+      f'opponent {opp_type}'
+    )
+
+lp.analyze(f)
--- no_fairy.py
+++ no_fairy.py
@@ -3,24 +3,24 @@ import lp
 import json
 import math
 
+with open('no_fairy_chart.json') as file:
+  multipliers = json.load(file)
+
+coefs = {}
+for att_type in multipliers:
+  coefs[att_type] = {
+    def_type: math.log2((multipliers[att_type][def_type] + .2)
+                        / (multipliers[def_type][att_type] + .2))
+    for def_type in multipliers
+  }
+
 def f(s):
-  with open('no_fairy_chart.json') as f:
-    multipliers = json.load(f)
-
-  coefs = {}
-  for att_type in multipliers:
-    coefs[att_type] = {
-      def_type: math.log2((multipliers[att_type][def_type] + .2)
-                          / (multipliers[def_type][att_type] + .2))
-      for def_type in multipliers
-    }
-
   x = {type: s.NumVar(0, s.infinity(), type) for type in coefs}
   e = s.NumVar(-s.infinity(), s.infinity(), 'E')
 
   s.Maximize(e)
 
-  s.Add(sum(x.values()) == 1)
+  s.Add(sum(x.values()) == 1, 'probabilities')
 
   for opp_type in coefs:
     s.Add(
