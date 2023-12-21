e3919b8a29227d6e9fb2d2f3ee92408f234ce1de
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-21 01:12:05 -0800

    whoops

--- pulp/analyze.py
+++ pulp/analyze.py
@@ -14,4 +14,4 @@ def analyze(p):
   print('Constraints')
   print(f'{"Name":16} {"Value":>12} {"Shadow price":>12} {"Bound":>12}')
   for c in p.constraints.values():
-    print(f'{c.name:16.16} {-c.constant - c.slack:12} {c.pi:12} {-c.constant:12}')
+    print(f'{c.name or "":16.16} {-c.constant - c.slack:12} {c.pi:12} {-c.constant:12}')
