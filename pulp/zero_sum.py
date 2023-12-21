84dcf1869bc42268922cde052f0b88bd39cbaf54
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-21 06:56:22 -0800

    c/

--- pulp/zero_sum.py
+++ pulp/zero_sum.py
@@ -9,9 +9,20 @@ p = pulp.LpProblem('z', pulp.LpMinimize)
 p += sum(x.values())
 
 shift = 42
+# shift = 25000
+C = (
+  (-41.5781+shift, 30.982+shift),
+  (7.942+shift, -5.918+shift)
+)
 
-p += sum(a*b for a, b in zip((-41.5781+shift, 30.982+shift), x.values())) >= 1
-p += sum(a*b for a, b in zip((7.942+shift, -5.918+shift), x.values())) >= 1
+# shift = 0
+# C = (
+#   (6000000+shift, 1000000+shift),
+#   (5000000+shift, 8000000+shift)
+# )
+
+for row in C:
+  p += sum(a*b for a, b in zip(row, x.values())) >= 1
 
 analyze(p)
 
