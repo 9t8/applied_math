8365eec46f570e230c9c5d8ff6a09fee0aa2df31
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-21 01:50:22 -0800

    h

--- pulp/analyze.py
+++ pulp/analyze.py
@@ -9,9 +9,9 @@ def analyze(p):
   print('Variables')
   print(f'{"Name":16} {"Value":>12} {"Reduced cost":>12} {"Coefficient":>12}')
   for v in p.variables():
-      print(f'{v.name:16.16} {pulp.value(v):12} {v.dj:12} {p.objective.get(v, 0):12}')
+      print(f'{v.name:16.16} {pulp.value(v):12.6} {v.dj:12} {p.objective.get(v, 0):12}')
 
   print('Constraints')
   print(f'{"Name":16} {"Value":>12} {"Shadow price":>12} {"Bound":>12}')
   for c in p.constraints.values():
-    print(f'{c.name or "":16.16} {-c.constant - c.slack:12} {c.pi:12} {-c.constant:12}')
+    print(f'{c.name or "":16.16} {-c.constant - c.slack:12} {c.pi:12.6} {-c.constant:12}')
