3a8b3fbc5f2aafbdede8b1cc04149cc16530cc4b
9t8 <63334933+9t8@users.noreply.github.com>
2023-12-21 02:01:41 -0800

    sdfa

--- pulp/analyze.py
+++ pulp/analyze.py
@@ -9,7 +9,7 @@ def analyze(p):
   print('Variables')
   print(f'{"Name":16} {"Value":>12} {"Reduced cost":>12} {"Coefficient":>12}')
   for v in p.variables():
-      print(f'{v.name:16.16} {pulp.value(v):12.6} {v.dj:12} {p.objective.get(v, 0):12}')
+      print(f'{v.name:16.16} {pulp.value(v):12.6} {v.dj:12.6} {p.objective.get(v, 0):12}')
 
   print('Constraints')
   print(f'{"Name":16} {"Value":>12} {"Shadow price":>12} {"Bound":>12}')
